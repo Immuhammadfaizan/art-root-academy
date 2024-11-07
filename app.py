6f0534756f2a70be6070f8ca88b07ff637dd8ddf
Muhammad Faizan <muhammadfaizanlite@gmail.com>
2024-11-07 18:10:55 +0500

    convert agree to boolean

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def register():
         prior_experience = request.form.get('prior_experience')
         experience_details = request.form.get('experience_details') if prior_experience == 'YES' else None
         payment_method = request.form.get('payment_method')
-        agree = request.form.get('agree')
+        agree = request.form.get('agree') = 'on'
 
         # Ensure courses is not empty, set it to None if no course is selected
         courses_str = ','.join(courses) if courses else None
